ff8599507e650a5981a0b457ef51832d94abd870
rk508501 <98303177+rk508501@users.noreply.github.com>
2023-05-04 15:55:55 +0530

    Cleanup

--- JS_Practice/index.js
+++ JS_Practice/index.js
@@ -1,35 +1,60 @@
+const express = require('express');
+const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 const { log } = require('console');
-var express = require('express');
-const { spawn } = require('child_process');
+const port = 3000;
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.text());
 
-app.use(express.json());
-
-app.get('/', function (request, response) {
-  response.send("Welcome to the VBScript Executor");    // echo the result back
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.post('/', function (request, response) {
-  console.log(request.body);     
-
-  // spawn the VBScript as a child process and pass the input values as command-line arguments
-  const scriptProcess = spawn('cscript.exe', [request]);
-
-  // capture the output of the VBScript
-  let output = '';
-  scriptProcess.stdout.on('data', (data) => {
-    output += data.toString();
-  });
-
-  // handle errors and exit codes
-  scriptProcess.on('error', (err) => {
-    console.error(`Error running VBScript: ${err}`);
-    res.status(500).json({ error: 'Internal server error' });
-  });
-
-  response.send(request.body);    // echo the result back
+app.get('/', (req, res) => {
+    const filePath = path.join('Readme/About.html');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error reading file');
+        } else {
+            res.send(data);
+        }
+    });
 });
 
-app.listen(3000, () => {
-  log("Listening at port 3000")
+app.post('/execute-vbscript', (req, res) => {
+    const script = req.body;
+    log("\n ---------------- Script received ----------------- \n" + script)
+
+    if (typeof script !== 'string') {
+        res.status(400).send('Script must be a string');
+        return;
+    }
+
+    // Write script contents to file
+    fs.writeFile('example.vbs', script, (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send(err.message);
+            return;
+        }
+
+        // Execute script using cscript
+        exec('cscript example.vbs', (err, stdout, stderr) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send(err.message);
+                return;
+            }
+
+            console.log(`\n Output from the script execution : ${stdout}`);
+            //console.error(`stderr: ${stderr}`);
+
+            // Return script output as response
+            res.send(stdout);
+        });
+    });
 });
